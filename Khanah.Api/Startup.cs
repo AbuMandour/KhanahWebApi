74eb0f4760a657c08cc0f0ff7292755e1e85832c
Muhammad Abu Mandour <muhammadabumandour@outlook.com>
2020-08-22 17:07:36 +0200

    add database connecion

--- Khanah.Api/Startup.cs
+++ Khanah.Api/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Khanah.Api.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,10 +28,12 @@ namespace Khanah.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<QuotesDBContext>(option => option.UseSqlServer(Configuration.GetConnectionString("QuotesContext")));
+            //Server=myServerAddress;Database=Quotes;User Id=sa;Password=mandour90  ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env , QuotesDBContext quotesDBContext)
         {
             if (env.IsDevelopment())
             {
@@ -42,6 +46,8 @@ namespace Khanah.Api
 
             //app.UseAuthorization();
 
+            var created = quotesDBContext.Database.EnsureCreated();
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
